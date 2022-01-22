ed451cb3f45d2a7499f70ef3eed473d6c5c40daf
DAI <senseye935@gmail.com>
2022-01-22 22:09:45 +0800

    fix todo.js

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,9 +4,27 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
 router.get('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
+  
+  return Todo.findOne({
+    where: { id, UserId }
+  })
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
@@ -46,17 +64,6 @@ router.put('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-router.get('/new', (req, res) => {
-  return res.render('new')
-})
 
-router.post('/', (req, res) => {
-  const UserId = req.user.id
-  const name = req.body.name
-
-  return Todo.create({ name, UserId })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 
 module.exports = router
