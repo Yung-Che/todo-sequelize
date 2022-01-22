df125dd0a69d59ad5e69a9c197842a814051531a
DAI <senseye935@gmail.com>
2022-01-22 21:51:49 +0800

    feat: add user authentication in todo and home routers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const usePassport = require('./config/passport')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 
@@ -21,10 +22,19 @@ app.use(session({
 }))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.use(routes)
 usePassport(app)
+
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
